4e609ba3b13f3b0a0e3f862013a39be2f4cf779c
ROKAYA <101093188+rokayamoghtet@users.noreply.github.com>
2022-04-29 23:23:07 +0100

    Items.java
    package com.sun.tools.javac.jvm

---
+++ Items.java
@@ -0,0 +1,13 @@
+package com.sun.tools.javac.jvm;
+
+public class Items {
+
+	public static int value;
+	public static int level;
+	public static int profit;
+	public static int weight;
+	public static double bound;
+
+ 
+	
+}
